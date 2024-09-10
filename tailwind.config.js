c20f1ffdfe7e33b795d67d260eeb2914f2f63c2b
arvin <rwiiinzakeri@gmail.com>
2024-09-10 14:04:52 +0330

    project

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        mainBlk: "#343434",
+        cartBlk: "#393939",
+        mainWhite: "#FFFFFF",
+        gold50: "#FAF3EB",
+        gold100: "#FAF3EB",
+        gold200: "#EAD1AE",
+        gold300: "#DFBA86",
+        gold400: "#D9AC6D",
+        gold500: "#D5A35D",
+        gold600: "#A2702A",
+        gold700: "#795420",
+        gold800: "#513815",
+        gold900: "#281C0B",
+        gold950: "#140E05",
+      },
+    },
+  },
+  plugins: [],
+};
